7258fecf670e724be2d5a2afee9aafc20aa087a6
Pamela Lam <pamela.le.lam@gmail.com>
2018-05-06 10:11:23 -0700

    fixed module exports and refactored tests

--- functions.js
+++ functions.js
@@ -1,67 +1,81 @@
-function checkRows(board) {
-  for (let i = 0; i < 3; i += 1) {
-    if (board[i][0] !== '' &&
-        board[i][0] === board[i][1] &&
-        board[i][0] === board[i][2]) {
-      return board[i][0]; // <-- return winner
+(function(){
+  function checkRows(board) {
+    for (let i = 0; i < 3; i += 1) {
+      if (board[i][0] !== '' &&
+          board[i][0] === board[i][1] &&
+          board[i][0] === board[i][2]) {
+        return board[i][0]; // <-- return winner
+      }
+    }
+  }
+      
+  function checkColumns(board) {
+    for (let j = 0; j < 3; j += 1) {
+      if (board[0][j] !== '' &&
+          board[0][j] === board[1][j] &&
+          board[0][j] === board[2][j]) {
+        return board[0][j]; // <-- return winner
+      }
+    }
+  }
+  
+  function checkDiagonals(board) {
+    //check \ diagonal
+    if (board[0][0] !== '' &&
+        board[0][0] === board [1][1] &&
+        board[0][0] === board [2][2]) {
+      return board[0][0]; // <-- return winner
+    }
+  
+    //check / diagonal
+    if (board[0][2] !== '' &&
+        board[0][2] === board [1][1] &&
+        board[0][2] === board [2][0]) {
+      return board[0][2]; // <-- return winner
+    }
+  }
+  
+  function boardFull(board) {
+    for (let i = 0; i < 3; i += 1) {
+      for (let j=0; j < 3; j += 1) {
+        if (board[i][j] === '') {
+          return false;
         }
       }
     }
-    
-function checkColumns(board) {
-  for (let j = 0; j < 3; j += 1) {
-    if (board[0][j] !== '' &&
-        board[0][j] === board[1][j] &&
-        board[0][j] === board[2][j]) {
-      return board[0][j]; // <-- return winner
-    }
+    return null;
   }
-}
-
-function checkDiagonals(board) {
-  //check \ diagonal
-  if (board[0][0] !== '' &&
-      board[0][0] === board [1][1] &&
-      board[0][0] === board [2][2]) {
-    return board[0][0]; // <-- return winner
-  }
-
-  //check / diagonal
-  if (board[0][2] !== '' &&
-      board[0][2] === board [1][1] &&
-      board[0][2] === board [2][0]) {
-    return board[0][2]; // <-- return winner
-  }
-}
-
-function boardFull(board) {
-  for (let i = 0; i < 3; i += 1) {
-    for (let j=0; j < 3; j += 1) {
-      if (board[i][j] === '') {
-        return false;
-      }
-    }
-  }
-  return null;
-}
-
-function setWinText(gameOver) {
-  let text = "";
   
-  if (gameOver === null) {
-    text = "CAT'S GAME";
-  } else if (gameOver === 'O') {
-    text = "OOPS! YOU LOST.";
-  } else if (gameOver === 'X') {
-    text = "NOT GOING TO HAPPEN"
+  function setWinText(gameOver) {
+    let text = "";
+    
+    if (gameOver === null) {
+      text = "CAT'S GAME";
+    } else if (gameOver === 'O') {
+      text = "OOPS! YOU LOST.";
+    } else if (gameOver === 'X') {
+      text = "NOT GOING TO HAPPEN"
+    }
+    return text;
   }
-  return text;
-}
 
-module.exports = { 
-  checkRows: checkRows,
-  checkColumns: checkColumns,
-  checkDiagonals: checkDiagonals,
-  boardFull: boardFull,
-  setWinText: setWinText
-};
+  if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = { 
+      checkRows: checkRows,
+      checkColumns: checkColumns,
+      checkDiagonals: checkDiagonals,
+      boardFull: boardFull,
+      setWinText: setWinText
+    };
+  } else {
+    window.checkRows = checkRows;
+    window.checkColumns = checkColumns;
+    window.checkDiagonals = checkDiagonals;
+    window.boardFull = boardFull;
+    window.setWinText = setWinText;
+  }
+
+})();
+
+
+
--- test/test.js
+++ test/test.js
@@ -1,40 +1,85 @@
 const assert = require('chai').assert;
 const f = require('../functions.js');
 
-const board = [
-  ['X','X','X'],
-  ['','',''],
-  ['','','']
-];
-
-const gameOver = null;
-
-describe('Rows', function(){
-  it('should return X', function(){
-    assert.equal(f.checkRows(board), 'X');
+describe('Rows', function() {
+  context('when X wins', function() {
+    const board = [
+      ['X','X','X'],
+      ['','O',''],
+      ['O','','']
+    ];
+    it('should return X', function() {
+      assert.equal(f.checkRows(board), 'X');
+    });
+  });
+  context('when O wins', function() {
+    const board = [
+      ['','X',''],
+      ['O','O','O'],
+      ['','X','']
+    ];
+    it('should return O', function() {
+      assert.equal(f.checkRows(board), 'O');
+    });
   });
 });
 
-describe('Columns', function(){
-  it('should return null', function(){
-    assert.equal(f.checkColumns(board), null);
+describe('Columns', function() {
+  context('when O wins', function() {
+    const board = [
+      ['O','X','X'],
+      ['O','','O'],
+      ['O','X','']
+    ];
+    it('should return null', function() {
+      assert.equal(f.checkColumns(board), 'O');
+    });
   });
 });
 
-describe('Diagonals', function(){
-  it('should return null', function(){
-    assert.equal(f.checkDiagonals(board), null);
+describe('Diagonals', function() {
+  context("when O wins \\", function() {
+    const board = [
+      ['O','X','X'],
+      ['','O',''],
+      ['O','X','O']
+    ];
+    it('should return O', function() {
+      assert.equal(f.checkDiagonals(board), 'O');
+    });
   });
 });
 
-describe('Board Full', function(){
-  it('should return false', function(){
-    assert.equal(f.boardFull(board), false);
+describe('Board Full', function() {
+  context('when the board is not full yet', function() {
+    const board = [
+      ['O','X','X'],
+      ['','O',''],
+      ['O','X','O']
+    ];
+    it('should return false', function() {
+      assert.equal(f.boardFull(board), false);
+    });
   });
 });
 
 describe('Set Result', function() {
-  it('should show tied result', function() {
-    assert.equal(f.setWinText(gameOver), "CAT'S GAME");
+  context('when no one wins', function() {
+    const gameOver = null;
+    it("should return CAT'S GAME", function() {
+      assert.equal(f.setWinText(gameOver), "CAT'S GAME");
+    });
+  });
+  context('when X wins', function() {
+    const gameOver = 'X';
+    it('should return NOT GOING TO HAPPEN', function() {
+      assert.equal(f.setWinText(gameOver), 'NOT GOING TO HAPPEN');
+    });
+  });
+  context('when O wins', function() {
+    const gameOver = 'O';
+    it('should return OOPS! YOU LOST.', function() {
+      assert.equal(f.setWinText(gameOver), 'OOPS! YOU LOST.');
+    });
   });
 });
